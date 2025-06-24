fd974b26ed0d53ed9ab29181247b7c8f11d4b377
Hilman <hilman@loyalto.id>
2025-06-24 16:41:10 +0700

    update: backend api

--- src/components/OnlyOfficeEditor.js
+++ src/components/OnlyOfficeEditor.js
@@ -20,7 +20,7 @@ const OnlyOfficeEditor = ({ file, onClose }) => {
 
             console.log('Loading config for file:', file.id);
             
-            const response = await axios.get(`http://localhost:3001/api/file/${file.id}`, {
+            const response = await axios.get(`https://a38c-36-84-233-118.ngrok-free.app/api/file/${file.id}`, {
                 timeout: 10000
             });
 
@@ -122,7 +122,7 @@ const OnlyOfficeEditor = ({ file, onClose }) => {
             setSaving(true);
             console.log('Saving changes for file:', file.id);
             
-            const response = await axios.post(`http://localhost:3001/api/save-changes`, {
+            const response = await axios.post(`https://a38c-36-84-233-118.ngrok-free.app/api/save-changes`, {
                 fileId: file.id,
                 fileName: file.name,
                 documentKey: config?.document?.key || ''
@@ -225,7 +225,8 @@ const OnlyOfficeEditor = ({ file, onClose }) => {
                     >
                         <DocumentEditor
                             id={`onlyoffice-editor-${file.id}`}
-                            documentServerUrl="http://localhost:8888/"
+                            // documentServerUrl="http://localhost:8888/"
+                            documentServerUrl="https://staging-onlyoffice.frackment.id"
                             config={config}
                             events_onDocumentReady={onDocumentReady}
                             events_onDocumentStateChange={onDocumentStateChange}
